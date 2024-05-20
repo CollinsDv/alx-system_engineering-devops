37b5ccf62031ae07e99ce31bd1b0a2cf0bccb79b
Collins Kimani <mwas9446.ck@gmail.com>
2024-05-20 23:28:08 +0300

    handle documention and PEP

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+"""module 1-export_to_CSV"""
+# Using what you did in the task #0,
+# extend your Python script to export data in the JSON format.
+
+#   Records all tasks that are owned by this employee
+#   Format must be: { "USER_ID": [
+#       {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+#           "username": "USERNAME"},
+#       {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+#           "username": "USERNAME"}, ... ]}
+#   File name must be: USER_ID.json
 import csv
 import requests
 import sys
 
 
 def fetch_todo_list(employee_id):
+    """fetches data from a URL and converts to CSV into a file"""
     todos_url = 'https://jsonplaceholder.typicode.com/todos'
     users_url = 'https://jsonplaceholder.typicode.com/users'
 
